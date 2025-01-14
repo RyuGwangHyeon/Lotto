94bb009e9c6347ab59bdd1263de26b4567e2784f
ryugwanghyoen <rgh4422@google.com>
2025-01-15 05:16:06 +0900

    refactor: 에러수정

--- lotto/src/libs/Lotto.tsx
+++ lotto/src/libs/Lotto.tsx
@@ -110,23 +110,3 @@ export const LottoLastDigitNumbers = () => {
 
   return numbers.sort((a: number, b: number) => a - b);
 };
-
-export const LottoEvenNumbers = () => {
-  let numbers: number[] = [];
-  let sum = 0;
-  let evenCount = 0;
-
-  while (true) {
-    numbers = Array.from(
-      { length: 6 },
-      () => Math.floor(Math.random() * 45) + 1
-    );
-    sum = numbers.reduce((acc: number, num: number) => acc + num, 0);
-    evenCount = numbers.filter(num => num % 2 === 0).length;
-    if (sum >= 100 && sum <= 175 && evenCount <= 3) {
-      break;
-    }
-  }
-
-  return numbers.sort((a: number, b: number) => a - b);
-};
