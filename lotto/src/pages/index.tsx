d1badc170c209aba626e6afd3e3a75e25a5244f4
ryugwanghyoen <rgh4422@google.com>
2025-05-27 08:55:01 +0900

    feat: 로직 다중선택 수정, favicon, title 수정

--- lotto/src/pages/index.tsx
+++ lotto/src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import LottoMachine from "@/components/LottoMachine";
 
 export default function Home() {
   return (
-    <div>
-      <LottoMachine />
-    </div>
+    <>
+      <Head>
+        <title>로또추첨기</title>
+        <link rel="icon" href="/favicon_v2.ico" />
+      </Head>
+      <div>
+        <LottoMachine />
+      </div>
+    </>
   );
 }
