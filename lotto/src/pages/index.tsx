62d02f04554d2b773a8e8d59c916eefb05efb3d9
ryugwanghyoen <rgh4422@google.com>
2025-05-27 10:42:18 +0900

    refactor:last

--- lotto/src/pages/index.tsx
+++ lotto/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import LottoMachine from "@/components/LottoMachine";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <title>로또추첨기</title>
+      <Head>
+        <title>로또추첨기</title>
+        <link rel="icon" href="/favicon2.ico" />
+      </Head>
       <div>
         <LottoMachine />
       </div>
